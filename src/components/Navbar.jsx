f2c5b233b525aacde7765904bd51f1b12f1e134c
dnlmkhlv <dan18082@mail.ru>
2024-09-03 15:39:47 +0400

    logo fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import logo from '../assets/logo/logo.svg';
+
 function Navbar() {
     return (
         <nav className="top-0">
             <div className="container mx-auto flex justify-between items-center p-4 lg:px-40">
                 <div className="flex items-center">
-                    <img src="src\assets\logo\logo.svg" alt="GymLabs Logo" className="w-[48px] sm:w-[48px] md:w-[56px] lg:w-[64px] max-w-full h-auto" />
+                    <img src={logo} alt="GymLabs Logo" className="w-[48px] sm:w-[48px] md:w-[56px] lg:w-[64px] max-w-full h-auto" />
                     <h1 className="ml-2 text-2xl font-bold">GymLabs AI</h1>
                 </div>
                 <div className="flex space-x-4">
